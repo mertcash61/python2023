98cb0a8cd2ee2f1557b67d0225006667cbc4f928
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2023-12-21 20:57:27 +0300

    Add files via upload

---
+++ python/matematik/matematikselişlemler.py
@@ -0,0 +1,17 @@
+# Toplama
+sayi1 = 5
+sayi2 = 3
+toplam = sayi1 + sayi2
+print("Toplam:", toplam)
+
+# Çıkarma
+fark = sayi1 - sayi2
+print("Fark:", fark)
+
+# Çarpma
+carpim = sayi1 * sayi2
+print("Çarpım:", carpim)
+
+# Bölme
+bolum = sayi1 / sayi2
+print("Bölüm:", bolum)
