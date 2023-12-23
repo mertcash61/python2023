c764ea0b3232b30623269c1ef984b2be038a03d9
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2023-12-23 13:11:05 +0300

    Add files via upload

---
+++ matematik/kullanıcıgirişi.py
@@ -0,0 +1,17 @@
+# Kullanıcıdan iki sayı girişi al
+sayi1 = float(input("Birinci saıyı girin:"))
+sayi2 = float(input("İkinci saıyı girin:"))
+sayi3 = float(input("Üçüncü saıyı girin:"))
+sayi4 = float(input("Dördüncü saıyı girin:"))
+sayi5 = float(input("Beşinci saıyı girin:"))
+sayi6 = float(input("Altıncı saıyı girin:"))
+sayi7 = float(input("Yedinci saıyı girin:"))
+sayi8 = float(input("Sekizinci saıyı girin:"))
+sayi9 = float(input("Dokuzuncu saıyı girin:"))
+sayi10 = float(input("Onuncu saıyı girin:"))
+
+# Toplamı hesapla
+toplam = sayi1 + sayi2 + sayi3 + sayi4 + sayi5 + sayi6 + sayi7 + sayi8 + sayi9 + sayi10
+
+# Sonucu ekrana yazdır
+print(f"{sayi1} + {sayi2} +{sayi3} + {sayi4} +{sayi5} + {sayi6} + {sayi7} + {sayi8} + {sayi9} + {sayi10}")
