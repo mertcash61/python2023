1d35cda93a3656c67980dc47a22eccd3431f8744
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2023-12-23 12:52:06 +0300

    Add files via upload

---
+++ matematik/4işlem.py
@@ -0,0 +1,21 @@
+# İki sayıyı topla
+sayi1 = 10
+sayi2 = 15
+toplam = sayi1 + sayi2
+print("Topla:",toplam)
+
+# Bir sayıyı diğerinden çıkar
+fark = sayi1 - sayi2
+print("Fark:",fark)
+
+# İki sayıyı çarp
+carpim = sayi1 * sayi2
+print("Çarpım:",carpim)
+
+# Bir sayıyı diğerine böl
+bolum = sayi1 / sayi2
+print("Bölüm:",bolum)
+
+# Bir sayının kuvvetini al
+us = sayi1 ** sayi2
+print("Üs:",us)
