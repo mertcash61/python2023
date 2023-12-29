30c78ab6b90aa98297b39401d533889c003e634f
mertcash61 <154323978+mertcash61@users.noreply.github.com>
2023-12-30 00:02:21 +0300

    Update

---
+++ phonenumber.py
@@ -0,0 +1,10 @@
+import phonenumber
+from phonenumbers import mertdoganay
+phone_number1 = phonenumbers.parse("")
+phone_number2 = phonenumbers.parse("")
+phone_number3 = phonenumbers.parse("")
+
+print("\nPhone Numbers Location\n")
+print(geocoder.description_for_numbers(phone_numbers1,"en"))
+print(geocoder.description_for_numbers(phone_numbers2,"en"))
+print(geocoder.description_for_numbers(phone_numbers3,"en"))
